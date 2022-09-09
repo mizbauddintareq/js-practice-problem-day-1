b5da09823913615caa3875c40ea4cfa75636f1f1
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-09-09 23:11:28 +0600

    solved all problems

---
+++ practice-problems.js
@@ -0,0 +1,30 @@
+// Problem no 1
+var momGives = 1000;
+var oneKgApplePrice = 89;
+var oneKgOrangePrice = 132;
+
+var remindMoney = momGives - oneKgApplePrice - oneKgOrangePrice;
+console.log(remindMoney);
+
+// Problem no 2
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bang = 99.5;
+
+var total = math + bio + chem + phy + bang;
+var average = parseFloat((total / 5).toFixed(2));
+
+console.log(average);
+
+// Problem no 3
+var firstName = "Mizba Uddin";
+var lastName = "Tareq";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Problem no 4
+var momGivesMoney = 119;
+var reminderBalance = 119 % 5;
+console.log(reminderBalance);
